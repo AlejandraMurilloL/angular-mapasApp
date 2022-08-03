20a40b00cf6061e924cefded0026fe5284b535a9
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-03 07:10:13 -0500

    Se crea el diseño del zoom range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+      .map-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .row {
+        background-color: white;
+        bottom: 50px;
+        left: 30px;
+        padding: 10px;
+        border-radius: 5px;
+        position: fixed;
+        z-index: 999;
+      }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+
+    const map = new mapboxgl.Map({
+      container: 'mapZoomRange', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-72.905406595323, 5.759048607794546], // starting position [lng, lat] 
+        zoom: 15, // starting zoom
+      });
+      map.on('style.load', () => {
+    });
   }
 
 }
