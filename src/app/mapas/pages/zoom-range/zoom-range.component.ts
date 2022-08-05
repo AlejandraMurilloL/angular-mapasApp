138e5a6c8a43552d30908d2016a26bf260534bf4
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-04 20:55:50 -0500

    Ajustes Zoom range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -24,11 +24,12 @@ import * as mapboxgl from 'mapbox-gl';
     `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('map') mapContainer!: ElementRef;
   map!: mapboxgl.Map;
   zoomLevel: number = 15;
+  center: [number, number] = [-72.905406595323, 5.759048607794546];
 
   constructor() { }
 
@@ -36,7 +37,7 @@ export class ZoomRangeComponent implements AfterViewInit {
       this.map = new mapboxgl.Map({
       container: this.mapContainer.nativeElement, // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-72.905406595323, 5.759048607794546], // starting position [lng, lat] 
+        center: this.center, // starting position [lng, lat] 
         zoom: this.zoomLevel, // starting zoom
       });
 
@@ -49,6 +50,17 @@ export class ZoomRangeComponent implements AfterViewInit {
           this.map.zoomTo(18);
         }
       });
+
+      this.map.on('move', (event) => {
+        const {lng, lat} =  event.target.getCenter();
+        this.center = [lng, lat];
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.map.off('zoom', () => {});
+    this.map.off('zoomend', () => {});
+    this.map.off('move', () => {});
   }
 
   zoomOut() {
